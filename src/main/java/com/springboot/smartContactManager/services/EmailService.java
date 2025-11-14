5f928b9d36ee3c18d05741d35d62a0a900c97193
HARSH SOLANKI <147095876+ranasaharshsingh@users.noreply.github.com>
2025-11-14 23:11:08 +0530

    Revoked personal Email and password from EmailService class

--- src/main/java/com/springboot/smartContactManager/services/EmailService.java
+++ src/main/java/com/springboot/smartContactManager/services/EmailService.java
@@ -29,8 +29,8 @@ public class EmailService {
         Session session = Session.getInstance(properties, new Authenticator(){
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                String username = "aitechtalks1@gmail.com";
-                 String password = "rvhl jpmg ccid hbpx";
+                String username = "";//enter the email to send otp
+                 String password = "";//enter the password or instance of password for sending mail
                 return new PasswordAuthentication(username, password);
             }
         });
